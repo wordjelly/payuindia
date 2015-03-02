ecfe306dc22a639b0d6f1068490e3e5995b26c12
Sandeep Kumar <sandeep.kumar@payu.in>
2015-03-02 13:40:40 +0530

    restrict syntax

--- lib/payu_india/action_view_helper.rb
+++ lib/payu_india/action_view_helper.rb
@@ -19,25 +19,29 @@ module PayuIndia #:nodoc:
     #       :html => { :id => 'payment-form' } %>
 
     def payment_form_for_payu(key, salt, options = {})
+      if !options.is_a?(Hash) || !key.is_a?(String) || !salt.is_a?(String)
+        concat("Something Wrong! params order -> key (String), salt (String), options (Hash) ")
+        nil
+      else
+        form_options = options.delete(:html) || {}
+        service = PayuIndia::Helper.new(key, salt, options)
+        result = []
 
-      form_options = options.delete(:html) || {}
-      service = PayuIndia::Helper.new(key, salt, options)
-      result = []
+        result << form_tag(PayuIndia.service_url, form_options.merge(:method => :post))
 
-      result << form_tag(PayuIndia.service_url, form_options.merge(:method => :post))
+        result << hidden_field_tag('key', key)
 
-      result << hidden_field_tag('key', key)
+        service.form_fields.each do |field, value|
+          result << hidden_field_tag(field, value)
+        end
 
-      service.form_fields.each do |field, value|
-        result << hidden_field_tag(field, value)
+        result << '<input type=submit value=" Pay with PayU ">'
+        result << '</form>'
+        result= result.join("\n")
+
+        concat(result.respond_to?(:html_safe) ? result.html_safe : result)
+        nil
       end
-
-      result << '<input type=submit value=" Pay with PayU ">'
-      result << '</form>'
-      result= result.join("\n")
-
-      concat(result.respond_to?(:html_safe) ? result.html_safe : result)
-      nil
     end
   end
 end
